772998b1b8d9fde7f5040e96ac56982f44552dfa
Edmir Galvan <edmirgalvaz@gmail.com>
2023-11-20 17:56:02 -0600

    Styles to InventoryPage

--- src/pages/InventoryPage/InventoryPage.tsx
+++ src/pages/InventoryPage/InventoryPage.tsx
@@ -1,7 +1,76 @@
+import "./InventoryPage.css";
+import { Navbar } from "../../components/Navbar/Navbar";
+import { ArrowDownIcon } from "../../components/icons";
+
 export function InventoryPage() {
   return (
-    <section className="inventoryPage">
-      <h1>Inventory Page</h1>
-    </section>
+    <>
+      <section className="inventoryPage">
+        <div className="inventoryPage--title">
+          <h1>Todo su inventario en un solo lugar</h1>
+          <small>Ultima actualización el 12/11/23 a las 15:06</small>
+        </div>
+        <button className="primaryButton">Nuevo producto</button>
+        <div className="inventoryPage--filters">
+          <h3>Ordenar por</h3>
+          <label>
+            Precio
+            <select>
+              <option value="1">Mayor a menor</option>
+              <option value="2">Menor a mayor</option>
+            </select>
+          </label>
+          <label>
+            Categoria
+            <select>
+              <option value="1">Categoria 1</option>
+              <option value="2">Categoria 2</option>
+            </select>
+          </label>
+          <label>
+            Nombre
+            <input type="text" />
+          </label>
+        </div>
+        <ul className="listOfProducts">
+          <li className="productCard">
+            <ArrowDownIcon />
+            <p>Nombre</p>
+            <small>Aleron</small>
+          </li>
+          <li className="productCard">
+            <ArrowDownIcon />
+            <p>Nombre</p>
+            <small>Aleron</small>
+          </li>
+          <li className="productCard">
+            <ArrowDownIcon />
+            <p>Nombre</p>
+            <small>Aleron</small>
+          </li>
+          <li className="productCard">
+            <ArrowDownIcon />
+            <p>Nombre</p>
+            <small>Aleron</small>
+          </li>
+          <li className="productCard">
+            <ArrowDownIcon />
+            <p>Nombre</p>
+            <small>Aleron</small>
+          </li>
+          <li className="productCard">
+            <ArrowDownIcon />
+            <p>Nombre</p>
+            <small>Aleron</small>
+          </li>
+          <li className="productCard">
+            <ArrowDownIcon />
+            <p>Nombre</p>
+            <small>Aleron</small>
+          </li>
+        </ul>
+      </section>
+      <Navbar />
+    </>
   );
 }
