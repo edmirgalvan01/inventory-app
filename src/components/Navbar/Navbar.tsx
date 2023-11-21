5bd97c277c57cf78de36d1fced1b95eca37d7680
Edmir Galvan <edmirgalvaz@gmail.com>
2023-11-20 19:01:02 -0600

    Create navbar distinct

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,23 +1,30 @@
 import "./Navbar.css";
 import { ClientsIcon, HomeIcon, InventoryIcon, SalesIcon } from "../icons";
 import { Link } from "react-router-dom";
+import { PATHNAMES } from "../../consts";
 
 export function Navbar() {
+  const getItemClassName = (pathname: string) => {
+    return window.location.pathname === pathname
+      ? "navbar--item active"
+      : "navbar--item";
+  };
+
   return (
     <nav className="navbar">
-      <Link to="/" className="navbar--item">
+      <Link to="/" className={getItemClassName(PATHNAMES.home)}>
         <HomeIcon />
         Inicio
       </Link>
-      <Link to="/inventory" className="navbar--item">
+      <Link to="/inventory" className={getItemClassName(PATHNAMES.inventory)}>
         <InventoryIcon />
         Inventario
       </Link>
-      <Link to="/sales" className="navbar--item">
+      <Link to="/sales" className={getItemClassName(PATHNAMES.sales)}>
         <SalesIcon />
         Ventas
       </Link>
-      <Link to="/clients" className="navbar--item">
+      <Link to="/clients" className={getItemClassName(PATHNAMES.clients)}>
         <ClientsIcon />
         Clientes
       </Link>
---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+export const PATHNAMES = {
+    home: "/",
+    inventory: "/inventory",
+    sales: "/sales",
+    clients: "/clients",
+  };
