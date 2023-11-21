6ff3124c08f781023360f9f9c79b892e848db66a
Edmir Galvan <edmirgalvaz@gmail.com>
2023-11-21 06:34:28 -0600

    Fix icons errors

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -7,27 +7,27 @@ export function HomeIcon() {
       viewBox="0 0 31 30"
       fill="none"
     >
-      <g clip-path="url(#clip0_9_3)">
+      <g clipPath="url(#clip0_9_3)">
         <path
           d="M7.10715 15H4.60715L15.8571 3.75L27.1071 15H24.6071"
           stroke="#F1F6F9"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M7.10715 15V23.75C7.10715 24.413 7.37054 25.0489 7.83938 25.5178C8.30822 25.9866 8.94411 26.25 9.60715 26.25H22.1071C22.7702 26.25 23.4061 25.9866 23.8749 25.5178C24.3438 25.0489 24.6071 24.413 24.6071 23.75V15"
           stroke="#F1F6F9"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M12.1071 26.25V18.75C12.1071 18.087 12.3705 17.4511 12.8394 16.9822C13.3082 16.5134 13.9441 16.25 14.6071 16.25H17.1071C17.7702 16.25 18.4061 16.5134 18.8749 16.9822C19.3438 17.4511 19.6071 18.087 19.6071 18.75V26.25"
           stroke="#F1F6F9"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </g>
       <defs>
@@ -53,28 +53,28 @@ export function InventoryIcon() {
       viewBox="0 0 31 30"
       fill="none"
     >
-      <g clip-path="url(#clip0_9_10)">
+      <g clipPath="url(#clip0_9_10)">
         <path
           d="M5.28571 10L15.2857 15L25.2857 10L15.2857 5L5.28571 10Z"
           stroke="#F1F6F9"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M15.2857 20L10.2857 17.5L5.28571 20L15.2857 25L25.2857 20L20.2857 17.5L15.2857 20Z"
           fill="#F1F6F9"
           stroke="#F1F6F9"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M10.2857 12.5L5.28571 15L10.2857 17.5M20.2857 17.5L25.2857 15L20.2857 12.5"
           stroke="#F1F6F9"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </g>
       <defs>
@@ -100,34 +100,34 @@ export function SalesIcon() {
       viewBox="0 0 31 30"
       fill="none"
     >
-      <g clip-path="url(#clip0_9_17)">
+      <g clipPath="url(#clip0_9_17)">
         <path
           d="M11.9643 6.25H9.46426C8.80122 6.25 8.16534 6.51339 7.6965 6.98223C7.22766 7.45107 6.96426 8.08696 6.96426 8.75V23.75C6.96426 24.413 7.22766 25.0489 7.6965 25.5178C8.16534 25.9866 8.80122 26.25 9.46426 26.25H21.9643C22.6273 26.25 23.2632 25.9866 23.732 25.5178C24.2009 25.0489 24.4643 24.413 24.4643 23.75V8.75C24.4643 8.08696 24.2009 7.45107 23.732 6.98223C23.2632 6.51339 22.6273 6.25 21.9643 6.25H19.4643"
           stroke="#F1F6F9"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M11.9643 6.25C11.9643 5.58696 12.2277 4.95107 12.6965 4.48223C13.1653 4.01339 13.8012 3.75 14.4643 3.75H16.9643C17.6273 3.75 18.2632 4.01339 18.732 4.48223C19.2009 4.95107 19.4643 5.58696 19.4643 6.25C19.4643 6.91304 19.2009 7.54893 18.732 8.01777C18.2632 8.48661 17.6273 8.75 16.9643 8.75H14.4643C13.8012 8.75 13.1653 8.48661 12.6965 8.01777C12.2277 7.54893 11.9643 6.91304 11.9643 6.25Z"
           stroke="#F1F6F9"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M18.2143 13.75H15.0893C14.592 13.75 14.1151 13.9475 13.7634 14.2992C13.4118 14.6508 13.2143 15.1277 13.2143 15.625C13.2143 16.1223 13.4118 16.5992 13.7634 16.9508C14.1151 17.3025 14.592 17.5 15.0893 17.5H16.3393C16.8365 17.5 17.3135 17.6975 17.6651 18.0492C18.0167 18.4008 18.2143 18.8777 18.2143 19.375C18.2143 19.8723 18.0167 20.3492 17.6651 20.7008C17.3135 21.0525 16.8365 21.25 16.3393 21.25H13.2143"
           stroke="#F1F6F9"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M15.7143 21.25V22.5M15.7143 12.5V13.75"
           stroke="#F1F6F9"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </g>
       <defs>
@@ -153,27 +153,27 @@ export function ClientsIcon() {
       viewBox="0 0 31 30"
       fill="none"
     >
-      <g clip-path="url(#clip0_9_25)">
+      <g clipPath="url(#clip0_9_25)">
         <path
           d="M8.14285 26.25V23.75C8.14285 22.4239 8.66964 21.1521 9.60732 20.2145C10.545 19.2768 11.8168 18.75 13.1429 18.75H15.6429"
           stroke="#F1F6F9"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M28.1429 20C28.1429 25 25.0179 27.5 23.7679 27.5C22.5179 27.5 19.3929 25 19.3929 20C20.6429 20 22.5179 19.375 23.7679 18.125C25.0179 19.375 26.8929 20 28.1429 20Z"
           stroke="#F1F6F9"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M10.6429 8.75C10.6429 10.0761 11.1696 11.3479 12.1073 12.2855C13.045 13.2232 14.3168 13.75 15.6429 13.75C16.9689 13.75 18.2407 13.2232 19.1784 12.2855C20.1161 11.3479 20.6429 10.0761 20.6429 8.75C20.6429 7.42392 20.1161 6.15215 19.1784 5.21447C18.2407 4.27678 16.9689 3.75 15.6429 3.75C14.3168 3.75 13.045 4.27678 12.1073 5.21447C11.1696 6.15215 10.6429 7.42392 10.6429 8.75Z"
           stroke="#F1F6F9"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </g>
       <defs>
@@ -199,20 +199,20 @@ export function TrendingIcon() {
       viewBox="0 0 30 30"
       fill="none"
     >
-      <g clip-path="url(#clip0_2_546)">
+      <g clipPath="url(#clip0_2_546)">
         <path
           d="M3.75 21.25L11.25 13.75L16.25 18.75L26.25 8.75"
           stroke="#394867"
-          stroke-width="3"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="3"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M17.5 8.75H26.25V17.5"
           stroke="#394867"
-          stroke-width="3"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="3"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </g>
       <defs>
@@ -233,27 +233,27 @@ export function UserIcon() {
       viewBox="0 0 30 30"
       fill="none"
     >
-      <g clip-path="url(#clip0_2_564)">
+      <g clipPath="url(#clip0_2_564)">
         <path
           d="M10 8.75C10 10.0761 10.5268 11.3479 11.4645 12.2855C12.4021 13.2232 13.6739 13.75 15 13.75C16.3261 13.75 17.5979 13.2232 18.5355 12.2855C19.4732 11.3479 20 10.0761 20 8.75C20 7.42392 19.4732 6.15215 18.5355 5.21447C17.5979 4.27678 16.3261 3.75 15 3.75C13.6739 3.75 12.4021 4.27678 11.4645 5.21447C10.5268 6.15215 10 7.42392 10 8.75Z"
           stroke="#394867"
-          stroke-width="3"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="3"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M7.5 26.25V23.75C7.5 22.4239 8.02678 21.1521 8.96447 20.2145C9.90215 19.2768 11.1739 18.75 12.5 18.75H17.5"
           stroke="#394867"
-          stroke-width="3"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="3"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M18.75 23.75L21.25 26.25L26.25 21.25"
           stroke="#394867"
-          stroke-width="3"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="3"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </g>
       <defs>
@@ -274,34 +274,34 @@ export function ReportIcon() {
       viewBox="0 0 30 30"
       fill="none"
     >
-      <g clip-path="url(#clip0_2_576)">
+      <g clipPath="url(#clip0_2_576)">
         <path
           d="M11.25 6.25H8.75C8.08696 6.25 7.45107 6.51339 6.98223 6.98223C6.51339 7.45107 6.25 8.08696 6.25 8.75V23.75C6.25 24.413 6.51339 25.0489 6.98223 25.5178C7.45107 25.9866 8.08696 26.25 8.75 26.25H21.25C21.913 26.25 22.5489 25.9866 23.0178 25.5178C23.4866 25.0489 23.75 24.413 23.75 23.75V8.75C23.75 8.08696 23.4866 7.45107 23.0178 6.98223C22.5489 6.51339 21.913 6.25 21.25 6.25H18.75"
           stroke="#394867"
-          stroke-width="3"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="3"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M11.25 6.25C11.25 5.58696 11.5134 4.95107 11.9822 4.48223C12.4511 4.01339 13.087 3.75 13.75 3.75H16.25C16.913 3.75 17.5489 4.01339 18.0178 4.48223C18.4866 4.95107 18.75 5.58696 18.75 6.25C18.75 6.91304 18.4866 7.54893 18.0178 8.01777C17.5489 8.48661 16.913 8.75 16.25 8.75H13.75C13.087 8.75 12.4511 8.48661 11.9822 8.01777C11.5134 7.54893 11.25 6.91304 11.25 6.25Z"
           stroke="#394867"
-          stroke-width="3"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="3"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M17.5 13.75H14.375C13.8777 13.75 13.4008 13.9475 13.0492 14.2992C12.6975 14.6508 12.5 15.1277 12.5 15.625C12.5 16.1223 12.6975 16.5992 13.0492 16.9508C13.4008 17.3025 13.8777 17.5 14.375 17.5H15.625C16.1223 17.5 16.5992 17.6975 16.9508 18.0492C17.3025 18.4008 17.5 18.8777 17.5 19.375C17.5 19.8723 17.3025 20.3492 16.9508 20.7008C16.5992 21.0525 16.1223 21.25 15.625 21.25H12.5"
           stroke="#394867"
-          stroke-width="3"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="3"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M15 21.25V22.5M15 12.5V13.75"
           stroke="#394867"
-          stroke-width="3"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="3"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </g>
       <defs>
@@ -322,48 +322,48 @@ export function TrophyIcon() {
       viewBox="0 0 30 30"
       fill="none"
     >
-      <g clip-path="url(#clip0_5_590)">
+      <g clipPath="url(#clip0_5_590)">
         <path
           d="M10 26.25H20"
           stroke="#394867"
-          stroke-width="3"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="3"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M15 21.25V26.25"
           stroke="#394867"
-          stroke-width="3"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="3"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M8.75 5H21.25"
           stroke="#394867"
-          stroke-width="3"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="3"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M21.25 5V15C21.25 16.6576 20.5915 18.2473 19.4194 19.4194C18.2473 20.5915 16.6576 21.25 15 21.25C13.3424 21.25 11.7527 20.5915 10.5806 19.4194C9.40848 18.2473 8.75 16.6576 8.75 15V5"
           stroke="#394867"
-          stroke-width="3"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="3"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M3.75 11.25C3.75 11.913 4.01339 12.5489 4.48223 13.0178C4.95107 13.4866 5.58696 13.75 6.25 13.75C6.91304 13.75 7.54893 13.4866 8.01777 13.0178C8.48661 12.5489 8.75 11.913 8.75 11.25C8.75 10.587 8.48661 9.95107 8.01777 9.48223C7.54893 9.01339 6.91304 8.75 6.25 8.75C5.58696 8.75 4.95107 9.01339 4.48223 9.48223C4.01339 9.95107 3.75 10.587 3.75 11.25Z"
           stroke="#394867"
-          stroke-width="3"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="3"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M21.25 11.25C21.25 11.913 21.5134 12.5489 21.9822 13.0178C22.4511 13.4866 23.087 13.75 23.75 13.75C24.413 13.75 25.0489 13.4866 25.5178 13.0178C25.9866 12.5489 26.25 11.913 26.25 11.25C26.25 10.587 25.9866 9.95107 25.5178 9.48223C25.0489 9.01339 24.413 8.75 23.75 8.75C23.087 8.75 22.4511 9.01339 21.9822 9.48223C21.5134 9.95107 21.25 10.587 21.25 11.25Z"
           stroke="#394867"
-          stroke-width="3"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="3"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </g>
       <defs>
@@ -384,7 +384,7 @@ export function ArrowDownIcon() {
       viewBox="0 0 30 30"
       fill="none"
     >
-      <g clip-path="url(#clip0_9_889)">
+      <g clipPath="url(#clip0_9_889)">
         <path
           fill-rule="evenodd"
           clip-rule="evenodd"
