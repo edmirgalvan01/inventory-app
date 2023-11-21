be3ce9d7ec696ccca7479fa9f01f8a979081cd87
Edmir Galvan <edmirgalvaz@gmail.com>
2023-11-20 19:50:14 -0600

    Create NewSalePage

---
+++ src/pages/NewSalePage/NewSalePage.tsx
@@ -0,0 +1,40 @@
+import "./NewSalePage.css";
+import { FieldInput } from "../../components/Field/Field";
+import { Navbar } from "../../components/Navbar/Navbar";
+import { PrimaryButton } from "../../components/buttons/buttons";
+
+export function NewSalePage() {
+  return (
+    <>
+      <section className="newSalePage">
+        <h1>Nuevo producto</h1>
+        <form className="newSalePage--form">
+          <FieldInput
+            label="Articulo"
+            onChange={() => {}}
+            placeholder="Aleron delantero"
+          />
+          <FieldInput
+            type="number"
+            label="Total"
+            onChange={() => {}}
+            placeholder="$132.4"
+          />
+          <FieldInput
+            type="number"
+            label="Cantidad vendida"
+            onChange={() => {}}
+            placeholder="12"
+          />
+          <FieldInput
+            label="Cliente"
+            onChange={() => {}}
+            placeholder="Edmir Galvan"
+          />
+          <PrimaryButton onClick={() => {}}>Guardar venta</PrimaryButton>
+        </form>
+      </section>
+      <Navbar />
+    </>
+  );
+}
