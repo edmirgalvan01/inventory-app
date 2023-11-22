96e1b38cd582af9a60158daaa9f245432c543471
Edmir Galvan <edmirgalvaz@gmail.com>
2023-11-22 16:11:28 -0600

    Create Clients filters

--- src/pages/ClientsPage/ClientsPage.tsx
+++ src/pages/ClientsPage/ClientsPage.tsx
@@ -1,14 +1,24 @@
 import "./ClientsPage.css";
+import { useState, ChangeEvent } from "react";
 import { ListOfClients } from "../../components/ListOfItems/ListOfItems";
 import { Navbar } from "../../components/Navbar/Navbar";
 import { PrimaryButton } from "../../components/buttons/buttons";
 import { ClientsTitle } from "./components/ClientsTitle";
-import { ClientsFilters } from "./components/ClientsFilters";
 import { useNavigate } from "react-router-dom";
 import initialClients from "../../mocks/clients.json";
 
 export function ClientsPage() {
   const navigate = useNavigate();
+
+  const [name, setName] = useState("");
+
+  const handleChangeName = (e: ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  const filteredClients = initialClients.filter((client) =>
+    client.name.includes(name)
+  );
 
   return (
     <>
@@ -17,8 +27,18 @@ export function ClientsPage() {
         <PrimaryButton onClick={() => navigate("/new-client")}>
           Nuevo cliente
         </PrimaryButton>
-        <ClientsFilters />
-        <ListOfClients clients={initialClients} />
+        <div className="clientsPage--filters">
+          <h3>Buscar</h3>
+          <label>
+            Nombre
+            <input
+              type="text"
+              placeholder="Edmir Galvan..."
+              onChange={handleChangeName}
+            />
+          </label>
+        </div>
+        <ListOfClients clients={filteredClients} />
       </section>
       <Navbar />
     </>
