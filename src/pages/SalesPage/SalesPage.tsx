3fce6640a8fe8309d6a2c82a489a0b4fb6184328
Edmir Galvan <edmirgalvaz@gmail.com>
2023-11-22 07:43:19 -0600

    Refactor SalesPage

--- src/pages/SalesPage/SalesPage.tsx
+++ src/pages/SalesPage/SalesPage.tsx
@@ -2,8 +2,6 @@ import "./SalesPage.css";
 import { PrimaryButton } from "../../components/buttons/buttons";
 import { ListOfSales } from "../../components/ListOfItems/ListOfItems";
 import { Navbar } from "../../components/Navbar/Navbar";
-import { SalesTitle } from "./components/SalesTitle";
-import { SalesFilters } from "./components/SalesFilters";
 import { useNavigate } from "react-router-dom";
 import initialSales from "../../mocks/sales.json";
 
@@ -13,15 +11,27 @@ export function SalesPage() {
   return (
     <>
       <section className="salesPage containerPage">
-        <SalesTitle />
-        <PrimaryButton
-          onClick={() => {
-            navigate("/new-sale");
-          }}
-        >
+        <div className="salesPage--title">
+          <h1>Todas sus ventas en un solo lugar</h1>
+          <small>Ultima actualización el 12/11/23 a las 15:06</small>
+        </div>
+        <PrimaryButton onClick={() => navigate("/new-sale")}>
           Nueva venta
         </PrimaryButton>
-        <SalesFilters />
+        <div className="salesPage--filters">
+          <h3>Buscar</h3>
+          <label>
+            Categoria
+            <select>
+              <option value="1">Categoria 1</option>
+              <option value="2">Categoria 2</option>
+            </select>
+          </label>
+          <label>
+            Fecha
+            <input type="date" />
+          </label>
+        </div>
         <ListOfSales sales={initialSales} />
       </section>
       <Navbar />
--- src/pages/SalesPage/components/SalesTitle.tsx
+++
@@ -1,8 +0,0 @@
-export function SalesTitle() {
-  return (
-    <div className="salesPage--title">
-      <h1>Todas sus ventas en un solo lugar</h1>
-      <small>Ultima actualización el 12/11/23 a las 15:06</small>
-    </div>
-  );
-}
