e2dbd482fb3d5ea1f2e193efbca20472d7ed048e
Edmir Galvan <edmirgalvaz@gmail.com>
2023-11-22 19:04:21 -0600

    Refactor to InventoryPage

--- src/pages/InventoryPage/InventoryPage.tsx
+++ src/pages/InventoryPage/InventoryPage.tsx
@@ -3,44 +3,19 @@ import { Navbar } from "../../components/Navbar/Navbar";
 import { ListOfProducts } from "../../components/ListOfItems/ListOfItems";
 import { PrimaryButton } from "../../components/buttons/buttons";
 import { useNavigate } from "react-router-dom";
-import { ChangeEvent, useState } from "react";
+import { useState } from "react";
 import { PageTitle } from "../../components/PageTitle/PageTitle";
-import { ProductType } from "../../types";
 import { useGetProducts } from "../../hooks/useGetProducts";
 
 export function InventoryPage() {
   const navigate = useNavigate();
+
   const { products, error } = useGetProducts();
+  const [category, setCategory] = useState("all");
 
-  const [filters, setFilters] = useState({
-    minPrice: 0,
-    category: "all",
+  const filterProducts = products.filter((product) => {
+    return category === "all" || product.category === category;
   });
-
-  const handleChangeCategory = (e: ChangeEvent<HTMLSelectElement>) => {
-    setFilters({
-      ...filters,
-      category: e.target.value,
-    });
-  };
-
-  const handleChangeMinPrice = (e: ChangeEvent<HTMLInputElement>) => {
-    const fixedValue = parseInt(e.target.value);
-
-    setFilters({
-      ...filters,
-      minPrice: fixedValue,
-    });
-  };
-
-  const filterProducts = (products: Array<ProductType>) => {
-    return products.filter((product) => {
-      return (
-        product.price >= filters.minPrice &&
-        (filters.category === "all" || product.category === filters.category)
-      );
-    });
-  };
 
   return (
     <>
@@ -53,27 +28,17 @@ export function InventoryPage() {
           <h3>Ordenar por</h3>
           <label>
             Categoria
-            <select onChange={handleChangeCategory}>
+            <select onChange={(e) => setCategory(e.target.value)}>
               <option value="all">Todas</option>
-              <option value="Literatura">Categoria 1</option>
-              <option value="Hogar">Categoria 2</option>
+              <option value="Pintura">Pintura</option>
+              <option value="Herramienta">Herramienta</option>
             </select>
-          </label>
-          <label>
-            Precio minimo
-            <input
-              type="range"
-              min={0}
-              max={15000}
-              onChange={handleChangeMinPrice}
-            />
-            <small>${filters.minPrice}</small>
           </label>
         </div>
         {error ? (
           <p>Hubo un error {error}</p>
         ) : (
-          <ListOfProducts products={filterProducts(products)} />
+          <ListOfProducts products={filterProducts} />
         )}
       </section>
       <Navbar />
