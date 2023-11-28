c67e5f65b24b19723ae94ab157a0b918ff6bc732
Edmir Galvan <edmirgalvaz@gmail.com>
2023-11-28 17:25:12 -0600

    Add EditProduct styles

--- src/pages/EditProduct/EditProduct.tsx
+++ src/pages/EditProduct/EditProduct.tsx
@@ -4,6 +4,7 @@ import { useGetProductById } from "../../hooks/useGetProductById";
 import { PrimaryButton } from "../../components/buttons/buttons";
 import { FieldInput } from "../../components/Field/Field";
 import { useUpdateProduct } from "../../hooks/useUpdateProduct";
+import { Navbar } from "../../components/Navbar/Navbar";
 
 export function EditProduct() {
   const { productId } = useParams();
@@ -12,33 +13,36 @@ export function EditProduct() {
   const { updateNewProduct, handleSubmit } = useUpdateProduct(product!);
 
   return (
-    <section className="editProduct containerPage">
-      <h1>Edita tu producto</h1>
-      <form onSubmit={handleSubmit}>
-        <FieldInput
-          label="Nombre"
-          placeholder={product?.name}
-          onChange={(e) => updateNewProduct("name", e.target.value)}
-        />
-        <FieldInput
-          label="Precio"
-          type="number"
-          placeholder={`$${product?.price.toString()}`}
-          onChange={(e) => updateNewProduct("price", e.target.value)}
-        />
-        <FieldInput
-          label="Stock"
-          type="number"
-          placeholder={product?.stock.toString()}
-          onChange={(e) => updateNewProduct("stock", e.target.value)}
-        />
-        <FieldInput
-          label="Categoria"
-          placeholder={product?.category}
-          onChange={(e) => updateNewProduct("category", e.target.value)}
-        />
-        <PrimaryButton>Editar producto</PrimaryButton>
-      </form>
-    </section>
+    <>
+      <section className="editProduct containerPage">
+        <h1>Edita tu producto</h1>
+        <form className="editProduct--form" onSubmit={handleSubmit}>
+          <FieldInput
+            label="Nombre"
+            placeholder={product?.name}
+            onChange={(e) => updateNewProduct("name", e.target.value)}
+          />
+          <FieldInput
+            label="Precio"
+            type="number"
+            placeholder={`$${product?.price.toString()}`}
+            onChange={(e) => updateNewProduct("price", e.target.value)}
+          />
+          <FieldInput
+            label="Stock"
+            type="number"
+            placeholder={product?.stock.toString()}
+            onChange={(e) => updateNewProduct("stock", e.target.value)}
+          />
+          <FieldInput
+            label="Categoria"
+            placeholder={product?.category}
+            onChange={(e) => updateNewProduct("category", e.target.value)}
+          />
+          <PrimaryButton>Editar producto</PrimaryButton>
+        </form>
+      </section>
+      <Navbar />
+    </>
   );
 }
