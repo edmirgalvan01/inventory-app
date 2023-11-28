33ebdece6a3b29fa1587687f397ce80d3a119b3b
Edmir Galvan <edmirgalvaz@gmail.com>
2023-11-28 17:11:55 -0600

    Create update method to Products

--- src/database/products.ts
+++ src/database/products.ts
@@ -1,3 +1,4 @@
+import { ProductType } from "../types";
 import { supabase } from "./supabase";
 
 export async function getProducts() {
@@ -15,6 +16,20 @@ export async function getProductById(id: string) {
     .from("products")
     .select("*")
     .eq("id", id);
+
+  if (error) {
+    return { success: false, error: error.message, data: null };
+  } else {
+    return { success: true, data: data, error: null };
+  }
+}
+
+export async function updateProduct(id: string, newProduct: ProductType) {
+  const { data, error } = await supabase
+    .from("products")
+    .update(newProduct)
+    .eq("id", id)
+    .select();
 
   if (error) {
     return { success: false, error: error.message, data: null };
